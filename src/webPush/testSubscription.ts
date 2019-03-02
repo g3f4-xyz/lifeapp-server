d872ee11a96c5be534553ce0aefefb18f56b29da
Kamil Grabek <kamil.grabek@allegro.pl>
2019-03-02 18:19:35 +0100

    testowe akce w powiadomieniu

--- src/webPush/testSubscription.ts
+++ src/webPush/testSubscription.ts
@@ -5,27 +5,33 @@ import { getSubscription } from '../db/api';
 export const testSubscription = async (ownerId: string, subscriptionModelId: string) => {
   try {
     const subscription = await getSubscription(ownerId, subscriptionModelId);
-    const { subscriptionData } = subscription;
 
+    const { subscriptionData } = subscription;
     const payload = JSON.stringify({
       title: 'Welcome to LifeApp!',
       notification: {
         body: 'This notification is test.' +
           ' It will be send always after entering page if notifications were allowed on this page.',
         icon: 'https://avatars2.githubusercontent.com/u/25178950?s=200&v=4',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1,
+        },
+        actions: [
+          {action: 'test', title: 'Test',
+            icon: 'https://avatars2.githubusercontent.com/u/25178950?s=200&v=4'},
+          {action: 'close', title: 'Close notification',
+            icon: 'https://avatars2.githubusercontent.com/u/25178950?s=200&v=4'},
+        ],
       },
     });
 
-    // console.log(['subscriptionData'], subscriptionData)
-
-    // Pass subscription into sendNotification
-    const { statusCode, body, headers } = await sendNotification(subscriptionData, payload);
-
-    console.log(['{ statusCode, body, headers }'], { statusCode, body, headers });
+    const { statusCode } = await sendNotification(subscriptionData, payload);
 
     return statusCode;
   } catch (error) {
-    console.log(['error'], error);
+    console.log(['testSubscription.error'], error);
     return error.statusCode;
   }
 };
