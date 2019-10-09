77e1bc197c037203a62a0f7d266a31b3aee2952c
Kamil Grabek <kamil.grabek@allegro.pl>
2019-10-09 17:03:20 +0200

    wywalenie stubów

--- src/services/__tests__/AppService.test.ts
+++ src/services/__tests__/AppService.test.ts
@@ -1,19 +1,30 @@
+import settingsApi from '../../db/api/settings/settingsApi';
+import taskApi from '../../db/api/task/taskApi';
 import AppService from '../AppService';
-import settingsApiStub from '../../stubs/settingsApiStub';
-import taskApiStub from '../../stubs/taskApiStub';
 
 describe('AppService', () => {
   it('should create new service', async () => {
-    const appService = new AppService(taskApiStub, settingsApiStub);
+    const appService = new AppService(taskApi, settingsApi);
 
     expect(appService).toBeDefined();
   });
 
-  it('should create new service', async () => {
+  it('should clean user data', async () => {
     const ownerId = '1234';
-    const deleteTasksSpy = jest.spyOn(taskApiStub, 'deleteTasks');
-    const deleteSettingsSpy = jest.spyOn(settingsApiStub, 'deleteSettings');
-    const appService = new AppService(taskApiStub, settingsApiStub);
+    const deleteTasksSpy = jest.spyOn(taskApi, 'deleteTasks').mockResolvedValue(ownerId);
+    const deleteSettingsSpy = jest.spyOn(settingsApi, 'deleteSettings').mockResolvedValue(ownerId);
+    const appService = new AppService(taskApi, settingsApi);
+
+    expect(await appService.cleanApplication(ownerId)).toBe(ownerId);
+    expect(deleteTasksSpy).toHaveBeenCalledWith(ownerId);
+    expect(deleteSettingsSpy).toHaveBeenCalledWith(ownerId);
+  });
+
+  it('should handle clean application error', async () => {
+    const ownerId = '1234';
+    const deleteTasksSpy = jest.spyOn(taskApi, 'deleteTasks').mockResolvedValue(ownerId);
+    const deleteSettingsSpy = jest.spyOn(settingsApi, 'deleteSettings').mockResolvedValue(ownerId);
+    const appService = new AppService(taskApi, settingsApi);
 
     expect(await appService.cleanApplication(ownerId)).toBe(ownerId);
     expect(deleteTasksSpy).toHaveBeenCalledWith(ownerId);
